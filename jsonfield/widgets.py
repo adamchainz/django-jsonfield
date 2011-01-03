bcddb9be0bf6ec430ac778810140bf4dadc229ef
Ian Lewis <IanMLewis@gmail.com>
2011-01-03 16:24:41 +0900

    Updated media for the JSONTableWidget so that it works on Django 1.2 without staticfiles installed

--- jsonfield/widgets.py
+++ jsonfield/widgets.py
@@ -17,9 +17,9 @@ class JSONSelectWidget(forms.SelectMultiple):
 class JSONTableWidget(JSONWidget):
     class Media:
         js = (
-            settings.STATIC_URL + 'js/jquery-1.4.4.js',
-            settings.STATIC_URL + 'js/jquery.tmpl.js',
-            settings.STATIC_URL + 'js/form2object.js',
-            settings.STATIC_URL + 'js/json-table.js',
-            settings.STATIC_URL + 'js/json-table-templates.js',
+            getattr(settings, 'STATIC_URL', settings.MEDIA_URL) + 'js/jquery-1.4.4.js',
+            getattr(settings, 'STATIC_URL', settings.MEDIA_URL) + 'js/jquery.tmpl.js',
+            getattr(settings, 'STATIC_URL', settings.MEDIA_URL) + 'js/form2object.js',
+            getattr(settings, 'STATIC_URL', settings.MEDIA_URL) + 'js/json-table.js',
+            getattr(settings, 'STATIC_URL', settings.MEDIA_URL) + 'js/json-table-templates.js',
         )
