14b7ccd984c91df903c35aafa68c6d478ee6e17b
Matthew Schinckel <matt@schinckel.net>
2012-01-16 15:58:26 +1030

    %z will be empty if naive anyway.

--- jsonfield/utils.py
+++ jsonfield/utils.py
@@ -3,6 +3,6 @@ from django.core.serializers.json import DjangoJSONEncoder
 
 class TZAwareJSONEncoder(DjangoJSONEncoder):
     def default(self, obj):
-        if isinstance(obj, datetime.datetime) and obj.tzinfo:
+        if isinstance(obj, datetime.datetime):
             return obj.strftime("%Y-%m-%d %H:%M:%S%z")
         return super(TZAwareJSONEncoder, self).default(obj)
