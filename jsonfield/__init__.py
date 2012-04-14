4f67eacc92d080235e1faafaa294f41f3aa1ffa2
Matthew Schinckel <matt@schinckel.net>
2012-04-14 13:33:16 +0930

    Just import JSONField now: setup.py no longer imports 'jsonfield'.

--- jsonfield/__init__.py
+++ jsonfield/__init__.py
@@ -1,8 +1,4 @@
 import os
 __version__ = open(os.path.join(os.path.dirname(__file__),'VERSION')).read().strip()
 
-# Temporary fix to not fail when django is not yet installed.
-try:
-    from fields import JSONField
-except ImportError:
-    pass
+from fields import JSONField
