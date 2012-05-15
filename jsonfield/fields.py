7618aa665a447f946817aa13c643556045385485
Michael Scheibe <mscheibe@gmail.com>
2012-05-15 13:24:35 -0700

    In django 1.3.1 django.utils.translation.trans_real._fetch loads all your apps in reverse order.
    Trying to import django.utils.trans_real.uggettext from django-jsonfield results in a circular import, specifically when another package imports django-jsonfield, like disqus's gargoyle.
    
    Using django.utils.translation.uggettext_lazy instead fixes the issue.

--- jsonfield/fields.py
+++ jsonfield/fields.py
@@ -1,7 +1,7 @@
 from django.core.exceptions import ValidationError
 from django.db import models
 from django.utils import simplejson as json
-from django.utils.translation import ugettext as _
+from django.utils.translation import ugettext_lazy as _
 
 from decimal import Decimal
 import datetime
