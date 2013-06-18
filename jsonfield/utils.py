bca52c4520ee3bc6636e474f79f6355ab87b7215
Matthew Schinckel <matt@schinckel.net>
2013-06-18 14:37:37 +0930

    Revert changes: freezegun has been updated.

--- jsonfield/utils.py
+++ jsonfield/utils.py
@@ -3,21 +3,9 @@ from decimal import Decimal
 
 from django.core.serializers.json import DjangoJSONEncoder
 
-DATETIME = (datetime.datetime,)
-DATE = (datetime.date,)
-TIME = (datetime.time,)
-
-try:
-    import freezegun.api
-except ImportError:
-    pass
-else:
-    DATETIME += (freezegun.api.FakeDatetime,)
-    DATE += (freezegun.api.FakeDate,)
-    
 class TZAwareJSONEncoder(DjangoJSONEncoder):
     def default(self, obj):
-        if isinstance(obj, DATETIME):
+        if isinstance(obj, datetime.datetime):
             return obj.strftime("%Y-%m-%d %H:%M:%S%z")
         return super(TZAwareJSONEncoder, self).default(obj)
 
@@ -26,13 +14,13 @@ def default(o):
         return o.to_json()
     if isinstance(o, Decimal):
         return str(o)
-    if isinstance(o, DATETIME):
+    if isinstance(o, datetime.datetime):
         if o.tzinfo:
             return o.strftime('%Y-%m-%dT%H:%M:%S%z')
         return o.strftime("%Y-%m-%dT%H:%M:%S")
-    if isinstance(o, DATE):
+    if isinstance(o, datetime.date):
         return o.strftime("%Y-%m-%d")
-    if isinstance(o, TIME):
+    if isinstance(o, datetime.time):
         if o.tzinfo:
             return o.strftime('%H:%M:%S%z')
         return o.strftime("%H:%M:%S")
