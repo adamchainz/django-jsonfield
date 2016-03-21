b702ff2a3985013acbd6a1a9a35471932361eeef
Adam Johnson <me@adamj.eu>
2016-03-21 12:34:29 +0000

    Tidy up __init__
    * Don't suppress ImportErrors from importing JSONfield - this lead to my build breaking because six wasn't installed, but I couldn't see why
    * Don't risk leaving the file pointer open to VERSION, use the context manager form of open to automatically close it

--- jsonfield/__init__.py
+++ jsonfield/__init__.py
@@ -1,7 +1,8 @@
 import os
-__version__ = open(os.path.join(os.path.dirname(__file__), 'VERSION')).read().strip()
 
-try:
-    from .fields import JSONField  # NOQA
-except ImportError:
-    pass
+from .fields import JSONField
+
+__all__ = ('JSONField',)
+
+with open(os.path.join(os.path.dirname(__file__), 'VERSION')) as fp:
+    __version__ = fp.read().strip()
