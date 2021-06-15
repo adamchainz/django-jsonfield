f18fd1f836c6de14f938295d37adc273d3f8537e
gabrielf-eb <53191391+gabrielf-eb@users.noreply.github.com>
2021-06-15 19:30:26 -0300

    [MINOR] fix repo release (#3)

--- tasks.py
+++ tasks.py
@@ -2,8 +2,8 @@ from invoke_release.tasks import *  # noqa: F403
 
 
 configure_release_parameters(  # noqa: F405
-    module_name='ebdjango',
-    display_name='Eventbrite Common Django Library',
+    module_name='jsonfield',
+    display_name='jsonfield Library',
     use_pull_request=True,
     use_tag=False,
 )
