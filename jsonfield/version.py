a21b050b09c76747d27ce8ee4890676ea7d1e93b
gabrielf-eb <53191391+gabrielf-eb@users.noreply.github.com>
2021-06-17 20:29:04 -0300

    Released jsonfield Library version 1.4.2+eventbrite (#8)
    Changelog Details:
    Python 3 compatibility

--- jsonfield/version.py
+++ jsonfield/version.py
@@ -1,2 +1,2 @@
-__version_info__ = (1, 4, 1)
-__version__ = '-'.join(filter(None, ['.'.join(map(str, __version_info__[:3])), (__version_info__[3:] or [None])[0]]))
+__version_info__ = (1, 4, 2, 'eventbrite')
+__version__ = '+'.join(filter(None, ['.'.join(map(str, __version_info__[:3])), (__version_info__[3:] or [None])[0]]))
